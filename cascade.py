a99e59ed07794a9bede47ea3e92f0de9689d26f0
TheSandwichCoder <athan5057@gmail.com>
2026-06-04 02:37:41 -0500

    first commit

---
+++ cascade.py
@@ -0,0 +1,179 @@
+
+import torch
+import torchvision
+from globals import device
+from tqdm import tqdm
+import numpy as np
+import time
+
+class CascadeClassifier:
+    def __init__(self, model, confidence_threshold=0.8, probability = 0, exec_time = 0, deterministic = False):
+        self.model = model
+        self.confidence_threshold = confidence_threshold
+
+        self.deterministic = deterministic
+        # TODO
+        self.probability = 0.0
+        self.exec_time = 0.0
+    
+    def predict_raw(self, x):
+        with torch.no_grad():
+            output = self.model(x)
+
+            probabilities = torch.nn.functional.softmax(output, dim=1)
+
+            top_prob, top_cat_id = torch.topk(probabilities, 1, dim=1)
+            # print(probabilities)
+            # print(top_cat_id)
+
+            return top_prob, top_cat_id
+
+    def predict(self, x):
+        with torch.no_grad():
+            output = self.model(x)
+
+            probabilities = torch.nn.functional.softmax(output, dim=1)
+            top_prob, top_cat_id = torch.topk(probabilities, 1, dim=1)
+
+            if self.deterministic:
+                return top_cat_id
+
+            # -1 is chosen as the id for IDK
+            idk_key = top_prob < self.confidence_threshold
+            top_cat_id[idk_key] = -1
+
+            return top_cat_id
+        
+def get_model_conf_threshold(cc, precision, validation_loader, eps = 0.0001):
+    all_predictions = torch.Tensor([])
+    all_confidences = torch.Tensor([])
+    all_labels = torch.Tensor([])
+
+    with torch.no_grad():
+        for batch_features, batch_labels in tqdm(validation_loader):
+            
+            probs, ids = cc.predict_raw(batch_features.to(device))
+            # print(all_predictions.shape, ids.cpu().shape, all_confidences.shape, probs.cpu().shape, all_labels.shape, batch_labels.cpu().shape)
+            
+            all_predictions = torch.cat((all_predictions, ids.cpu()), dim=0)
+            all_confidences = torch.cat((all_confidences, probs.cpu()), dim=0)
+            all_labels = torch.cat((all_labels, batch_labels.cpu()), dim=0)
+
+    all_predictions = np.array(all_predictions).flatten()
+    all_confidences = np.array(all_confidences).flatten()
+    all_labels = np.array(all_labels)
+
+    # precisions, recalls, thresholds = precision_recall_curve(all_labels, y_scores)
+
+    # # Define your target precision (e.g., 90%)
+    # target_precision = 0.90
+
+    # # Find all thresholds that meet or exceed the target precision
+    # valid_indices = np.where(precisions >= target_precision)[0]
+
+    # # Get the lowest threshold that hits the target (to maximize recall)
+    # best_threshold = thresholds[valid_indices[0]]
+
+
+
+    low = 0
+    high = 1
+    mid = (low + high) / 2
+    last_successful = mid
+
+    # binary search for confidence threshold for precision
+    while low < high:
+        mid = (low + high) / 2
+        # pred correct
+        
+        num_correct = np.sum(((all_confidences >= mid) & (all_predictions == all_labels)))
+
+        # pred made
+        num_total = np.sum(all_confidences >= mid)
+
+        p = num_correct / num_total
+        print(p, mid)
+
+        if p >= precision:
+            high = mid - eps
+            last_successful = mid
+        else:
+            low = mid + eps
+
+
+    return last_successful
+
+
+def get_resnet_18():
+    weights = torchvision.models.ResNet18_Weights.DEFAULT
+    model = torchvision.models.resnet18(weights=weights).to(device)
+    model.eval()
+
+    return model
+    
+def get_resnet_34():
+    weights = torchvision.models.ResNet34_Weights.DEFAULT
+    model = torchvision.models.resnet34(weights=weights).to(device)
+    model.eval()
+
+    return model
+    
+def get_resnet_152():
+    weights = torchvision.models.ResNet152_Weights.DEFAULT
+    model = torchvision.models.resnet152(weights=weights).to(device)
+    model.eval()
+
+    return model
+    
+
+# no optimization. Run classifiers in order
+class IDKCascade:
+    def __init__(self, classifiers, dependent = True):
+        self.n_classifiers = len(classifiers)
+        self.classifiers = classifiers
+        self.is_dependent = dependent
+
+    # last classifier assumsed as deterministic
+    def predict(self, x):
+        for cc_i, cc in enumerate(self.classifiers):
+            pred = cc.predict(x)
+
+            if pred != -1 or cc_i == self.n_classifiers - 1:
+                return pred
+            
+    def get_expected_duration(self):
+        pass
+
+            
+def bench_classifier(cc, data_loader):
+    correct = 0
+    total_guessed = 0
+    total = 0
+    time_taken = 0
+
+    counter = 0
+    with torch.no_grad():
+        for batch_features, batch_label in tqdm(data_loader):
+            st = time.perf_counter()
+            pred = cc.predict(batch_features.to(device))
+            et = time.perf_counter()
+
+            correct += sum(pred.flatten() == batch_label.flatten())
+
+            total_guessed += sum(pred.flatten() != -1)
+            
+            print(pred.flatten(), batch_label.flatten())
+            total += len(pred)
+            time_taken += et - st
+            if counter > 100:
+                break
+            counter += 1
+
+    acc = correct / total
+    prec = correct / total_guessed
+    prob = total_guessed / total
+    avg_time = time_taken / total 
+
+    print(f"acc:{acc} prec:{prec} prob:{prob} avg time:{avg_time*1000:.1f}ms")
+
+    return acc, prec, prob, avg_time
---
+++ main.py
@@ -0,0 +1,63 @@
+
+import torch
+import torchvision.models
+from cascade import *
+from ImageNetReader import *
+
+from globals import device, DESIRED_PRECISION, M1_P95_CONF_THRESHOLD, M2_P95_CONF_THRESHOLD, M3_P95_CONF_THRESHOLD
+import torchvision.transforms as transforms
+
+ImageNetV2Transforms = preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], 
+        std=[0.229, 0.224, 0.225]
+    )
+])
+dataset = ImageNetV2Dataset(location="data", transform=ImageNetV2Transforms)
+loader = DataLoader(dataset, batch_size=16, num_workers=4)
+
+def get_confidence_thresholds():
+
+    m1 = get_resnet_18()
+    cc1 = CascadeClassifier(m1)
+
+    print(get_model_conf_threshold(cc1, DESIRED_PRECISION, loader))
+
+
+    m2 = get_resnet_34()
+    cc2 = CascadeClassifier(m2)
+
+    print(get_model_conf_threshold(cc2, DESIRED_PRECISION, loader))
+
+    m3 = get_resnet_152()
+    cc3 = CascadeClassifier(m3)
+
+    print(get_model_conf_threshold(cc3, DESIRED_PRECISION, loader))
+
+def benchmark():
+    m1 = get_resnet_18()
+    cc1 = CascadeClassifier(m1, M1_P95_CONF_THRESHOLD)
+
+    m2 = get_resnet_34()
+    cc2 = CascadeClassifier(m2, M2_P95_CONF_THRESHOLD)
+
+    m3 = get_resnet_152()
+    cc3 = CascadeClassifier(m3, M3_P95_CONF_THRESHOLD, deterministic=True)
+
+    cascade = IDKCascade([cc1, cc2, cc3])
+
+    bench_classifier(cc1, loader)
+    bench_classifier(cc2, loader)
+    bench_classifier(cc3, loader)
+    bench_classifier(cascade, loader)
+
+
+# m3 = get_resnet_152()
+# cc3 = CascadeClassifier(m3, M3_P95_CONF_THRESHOLD)
+# bench_classifier(cc3, loader)
+
+get_confidence_thresholds()
+benchmark()
